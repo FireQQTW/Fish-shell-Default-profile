39406b576ceec4dcc55efe38b670f47ca3ff5535
Derek <azraelxuan927@gmail.com>
2014-04-08 09:46:20 +0800

    change config.fish

--- config.fish
+++ config.fish
@@ -1,8 +1,9 @@
 # Update the PATH to ensure Homebrew packages are used.
-set homebrew  "/usr/local/bin"
-set -gx PATH $homebrew $PATH
+#set homebrew  "/usr/bin"
+#set -gx PATH $homebrew $PATH
 
 # Set architecture flags
 set -gx ARCHFLAGS "-arch x86_64"
 # Enable the use of the `help` command.
 set BROWSER open
+set -gx PATH (brew --prefix ruby)/bin $PATH
